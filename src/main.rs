a3a5304d4d5478d144cd8f66896fae6c418e1109
Jun Woo Shin <shin.junwoo97@gmail.com>
2021-01-21 21:47:53 -0800

    warp

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,17 @@
-fn main() {
-    println!("Hello, world!");
+use warp::Filter;
+
+#[tokio::main]
+async fn main() {
+    pretty_env_logger::init();
+
+    let log = warp::log("exchange");
+
+    // GET /hello/warp => 200 OK with body "Hello, warp!"
+    let hello = warp::path!("hello" / String)
+        .map(|name| format!("Hello, {}!", name))
+        .with(log);
+
+    warp::serve(hello)
+        .run(([127, 0, 0, 1], 3030))
+        .await;
 }
