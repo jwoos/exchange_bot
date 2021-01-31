daa41c9f3be56c8b2815e6e4e299e4e232f70540
Jun Woo Shin <shin.junwoo97@gmail.com>
2021-01-30 18:59:32 -0800

    reimplement url_verification

--- src/api.rs
+++ src/api.rs
@@ -1,11 +1,10 @@
 use warp::Filter;
 
 use crate::handlers;
-use crate::slack::{event, event_wrapper};
 
 pub fn compose_api(
 ) -> impl warp::Filter<Extract = impl warp::Reply, Error = warp::Rejection> + Clone {
-    status().or(events()).or(events_url_verification())
+    status().or(events())
 }
 
 fn status() -> impl warp::Filter<Extract = impl warp::Reply, Error = warp::Rejection> + Clone {
@@ -14,64 +13,9 @@ fn status() -> impl warp::Filter<Extract = impl warp::Reply, Error = warp::Rejec
         .and_then(handlers::status)
 }
 
-fn post_json_filter(
-) -> impl warp::Filter<Extract = (serde_json::Value,), Error = warp::Rejection> + Clone {
-    warp::post().and(warp::body::json())
-}
-
-fn events_url_verification_filter(
-) -> impl warp::Filter<Extract = (event::url_verification::UrlVerification,), Error = warp::Rejection>
-       + Clone {
-    post_json_filter().and_then(|body: serde_json::value::Value| async move {
-        let opt = body
-            .get("type")
-            .and_then(|event_type: &serde_json::Value| event_type.as_str())
-            .and_then(|event_type: &str| {
-                if event_type == "url_verification" {
-                    return Some(());
-                } else {
-                    return None;
-                }
-            });
-
-        if opt.is_none() {
-            return Err(warp::reject::reject());
-        } else {
-            return serde_json::from_value(body).or(Err(warp::reject::reject()));
-        }
-    })
-}
-
-fn events_filter(
-) -> impl warp::Filter<Extract = (event_wrapper::EventWrapper,), Error = warp::Rejection> + Clone {
-    post_json_filter().and_then(|body: serde_json::value::Value| async move {
-        let opt = body
-            .get("type")
-            .and_then(|event_type: &serde_json::Value| event_type.as_str())
-            .and_then(|event_type: &str| {
-                if event_type == "event_callback" {
-                    return Some(());
-                } else {
-                    return None;
-                }
-            });
-
-        if opt.is_none() {
-            return Err(warp::reject::reject());
-        } else {
-            return serde_json::from_value(body).or(Err(warp::reject::reject()));
-        }
-    })
-}
-
-fn events_url_verification(
-) -> impl warp::Filter<Extract = impl warp::Reply, Error = warp::Rejection> + Clone {
-    warp::path!("slack" / "events")
-        .and(events_url_verification_filter())
-        .and_then(handlers::events_url_verification)
-}
-
-// TODO fix
 fn events() -> impl warp::Filter<Extract = impl warp::Reply, Error = warp::Rejection> + Clone {
-    warp::path!("slack" / "events").and(post_json_filter().and_then(handlers::echo))
+    warp::path!("slack" / "events")
+        .and(warp::post())
+        .and(warp::body::json())
+        .and_then(handlers::events)
 }
--- src/handlers.rs
+++ src/handlers.rs
@@ -22,8 +22,50 @@ pub async fn events_url_verification(
     ))
 }
 
-pub async fn events(event: event_wrapper::EventWrapper) -> Result<impl warp::Reply, Infallible> {
-    Ok(warp::reply::json(
-        &serde_json::json!({"status": "received"}),
+// TODO create a response struct and impl Serialize on it and use that to return
+pub async fn events(event: serde_json::Value) -> Result<impl warp::Reply, Infallible> {
+    let event_type_opt = event
+        .get("type")
+        .and_then(|event_type: &serde_json::Value| event_type.as_str());
+
+    if let Some(event_type) = event_type_opt {
+        match event_type {
+            "url_verification" => {
+                if let Ok(url_verification) =
+                    serde_json::from_value::<event::url_verification::UrlVerification>(event)
+                {
+                    return Ok(warp::reply::with_status(
+                        warp::reply::json(
+                            &serde_json::json!({"challenge": url_verification.get_challenge()}),
+                        ),
+                        http::status::StatusCode::OK,
+                    ));
+                } else {
+                    return Ok(warp::reply::with_status(
+                        warp::reply::json(
+                            &serde_json::json!({"message": "Invalid event callback"}),
+                        ),
+                        http::status::StatusCode::BAD_REQUEST,
+                    ));
+                }
+            }
+            "event_callback" => {
+                return Ok(warp::reply::with_status(
+                    warp::reply::json(&serde_json::json!({"message": "Not implemented"})),
+                    http::status::StatusCode::BAD_REQUEST,
+                ))
+            }
+            _ => {
+                return Ok(warp::reply::with_status(
+                    warp::reply::json(&serde_json::json!({"message": "Invalid message"})),
+                    http::status::StatusCode::BAD_REQUEST,
+                ))
+            }
+        }
+    }
+
+    Ok(warp::reply::with_status(
+        warp::reply::json(&serde_json::json!({"message": "Invalid message"})),
+        http::status::StatusCode::BAD_REQUEST,
     ))
 }
