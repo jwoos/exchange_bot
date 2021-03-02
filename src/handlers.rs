c11637e6744b4965a51d2ec3025b24de23b6d50f
Jun Woo Shin <shin.junwoo97@gmail.com>
2021-03-01 21:01:04 -0800

    address TODO in handler

--- src/handlers.rs
+++ src/handlers.rs
@@ -16,7 +16,21 @@ pub async fn status() -> Result<impl warp::Reply, Infallible> {
     Ok(warp::reply::json(&Status { status: "okay" }))
 }
 
-// TODO create a response struct and impl Serialize on it and use that to return
+struct EventsError<'a> {
+    message: &'a str,
+}
+
+#[derive(serde::Serialize)]
+struct UrlVerificationResponse<'a> {
+    challenge: &'a str,
+}
+
+impl<'a> UrlVerificationResponse<'a> {
+    fn new(challenge: &'a str) -> UrlVerificationResponse {
+        UrlVerificationResponse { challenge }
+    }
+}
+
 pub async fn events(
     event: serde_json::Value,
     client: &reqwest::Client,
@@ -32,9 +46,9 @@ pub async fn events(
                     serde_json::from_value::<event::url_verification::UrlVerification>(event)
                 {
                     return Ok(warp::reply::with_status(
-                        warp::reply::json(
-                            &serde_json::json!({"challenge": url_verification.get_challenge()}),
-                        ),
+                        warp::reply::json(&UrlVerificationResponse::new(
+                            url_verification.get_challenge(),
+                        )),
                         http::status::StatusCode::OK,
                     ));
                 } else {
